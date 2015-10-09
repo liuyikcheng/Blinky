301ce8eb940eb43e318e6394f5ca4e5c57f38cb2
jasonchuah93 <jasonchuah1993@hotmail.com>
2015-10-09 15:47:49 +0800

    Created blinky project

---
+++ app/main.c
@@ -0,0 +1,26 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal_gpio.h"
+
+void configurePort(GPIO_TypeDef *port, uint32_t pinNumber){
+	GPIO_InitTypeDef gpio;
+	gpio.Pin = pinNumber;
+	gpio.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio.Speed = GPIO_SPEED_MEDIUM;
+	HAL_GPIO_Init(port, &gpio);
+}
+
+int main(void)
+{
+	int a = 4;
+	double d = 5.6789123;
+
+	d = d * a + 1.987656321;
+
+	configurePort(GPIOG, GPIO_PIN_13);
+
+	while(1)
+    {
+		HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, GPIO_PIN_RESET);
+    }
+}
