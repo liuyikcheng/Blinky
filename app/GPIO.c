cc91042797f5c80284c3d7c12a410000538a7632
Liu Yik Cheng <liuyikcheng@msn.com>
2015-10-26 14:44:18 +0800

    Creating gpioUnresetEnableClock function to enable ports manually

--- app/GPIO.c
+++ app/GPIO.c
@@ -1,14 +1,28 @@
 #include "GPIO.h"
 #include "stm32f4xx_hal_rcc.h"
 
-void configurePin(int direction, int pinNumber, GPIO_TypeDef *port){
+void configurePin(int mode, int pinNumber, GPIO *port){
 	GPIO_InitTypeDef gpio;
+	RCC_REG rcc;
 
-	__HAL_RCC_GPIOG_CLK_ENABLE();
-	__HAL_RCC_GPIOB_CLK_ENABLE();
-	__HAL_RCC_GPIOC_CLK_ENABLE();
+	//__HAL_RCC_GPIOG_CLK_ENABLE();
+	//__HAL_RCC_GPIOB_CLK_ENABLE();
+	//__HAL_RCC_GPIOC_CLK_ENABLE();
 
-	int *ptr = (int*)0x40021800;
+
+/*
+	int *ptr = (int*)0x40023810;
+	ptr[0] = 0xFFFFFFBF;
+	int *enPtr = (int*)0x40023830;
+	enPtr[0] = 0x00100040;*/
+
+	port->MODER &= ~(3 << (pinNumber * 2));
+	port->MODER |= mode << (pinNumber * 2);				// MODE.pin14 = output
+	port->OTYPER &= ~(1 << (pinNumber));
+	port->OSPEED &= ~(3 << (pinNumber * 2));
+	port->OSPEED |= GPIO_HIGH_SPEED << (pinNumber * 2);
+
+/*	int *ptr = (int*)0x40021800;
 	int *ptrB = (int*)0x40020400;
 	int *ptrC = (int*)0x40020800;
 
@@ -22,7 +36,7 @@ void configurePin(int direction, int pinNumber, GPIO_TypeDef *port){
 
 	ptrC[0] = 0x00000400;
 	ptrC[1] = 0;
-	ptrC[2] = 0x00000800;
+	ptrC[2] = 0x00000800;*/
 //	gpio.Pin = pinNumber;
 //	gpio.Mode = direction;
 //	gpio.Speed = GPIO_SPEED_MEDIUM;
@@ -30,6 +44,14 @@ void configurePin(int direction, int pinNumber, GPIO_TypeDef *port){
 //	HAL_GPIO_Init(port, &gpio);
 }
 
+void gpioUnresetEnableClock(GPIO *port){
+//	switch(port){
+//			case PORTG:
+//				rcc->AHB1RSTR = 0xFFFFFFBF;
+//				rcc->AHB1ENR  = 0x00100040;
+//				break;
+//		}
+}
 
 void writeOne(int pinNumber, GPIO_TypeDef *port){
 	HAL_GPIO_WritePin(port, pinNumber, GPIO_PIN_SET);
@@ -38,3 +60,5 @@ void writeOne(int pinNumber, GPIO_TypeDef *port){
 void writeZero(int pinNumber, GPIO_TypeDef *port){
 	HAL_GPIO_WritePin(port, pinNumber, GPIO_PIN_RESET);
 }
+
+
--- app/GPIO.h
+++ app/GPIO.h
@@ -6,7 +6,82 @@
 #define GPIO_OUTPUT		GPIO_MODE_OUTPUT_PP
 #define GPIO_INPUT		GPIO_MODE_INPUT_PP
 
-void configurePin(int direction, int pinNumber, GPIO_TypeDef *port);
+typedef struct GPIO_t GPIO;
+struct GPIO_t{
+	uint32_t MODER;
+	uint32_t OTYPER;
+	uint32_t OSPEED;
+	uint32_t PUPDR;
+	uint32_t IDR;
+	uint32_t ODR;
+	uint32_t BSRR;
+	uint32_t LCKR;
+	uint32_t AFRL;
+	uint32_t AFRH;
+
+};
+
+typedef struct RCC_t RCC_REG;
+struct RCC_t{
+	uint32_t CR;
+	uint32_t PLLCFGR;
+	uint32_t CFGR;
+	uint32_t CIR;
+	uint32_t AHB1RSTR;
+	uint32_t AHB2RSTR;
+	uint32_t AHB3RSTR;
+	uint32_t Reserved1;
+	uint32_t APB1RSTR;
+	uint32_t APB2RSTR;
+	uint32_t Reserved2;
+	uint32_t Reserved3;
+	uint32_t AHB1ENR;
+};
+
+#define CLK_ENABLE
+
+#define PIN_0		0
+#define PIN_1		1
+#define PIN_2		2
+#define PIN_3		3
+#define PIN_4		4
+#define PIN_5		5
+#define PIN_6		6
+#define PIN_7		7
+#define PIN_8		8
+#define PIN_9		9
+#define PIN_10		10
+#define PIN_11		11
+#define PIN_12		12
+#define PIN_13		13
+#define PIN_14		14
+#define PIN_15		15
+
+#define GPIO_MODE_INTPUT		0
+#define GPIO_MODE_OUTPUT		1
+#define GPIO_MODE_ALTFUNC		2
+#define GPIO_MODE_ANALOG		3
+
+#define GPIO_LOW_SPEED			0
+#define GPIO_MEDIUM_SPEED		1
+#define GPIO_HIGH_SPEED			2
+#define GPIO_VERY_HIGH_SPEED	3
+
+#define GPIOG_BASE_ADDRESS	0x40021800
+#define GPIOB_BASE_ADDRESS	0x40020400
+#define GPIOC_BASE_ADDRESS	0x40020800
+
+#define PORTG	((GPIO *)GPIOG_BASE_ADDRESS)
+#define PORTB	((GPIO *)GPIOB_BASE_ADDRESS)
+#define PORTC	((GPIO *)GPIOC_BASE_ADDRESS)
+
+#define RCC_REG_RESET_ADDRESS		0x40023810
+
+#define RCC_REG_RESET	((int *)RCC_REG_RESET_ADDRESS)
+
+void configurePin(int mode, int pinNumber, GPIO *port);
+void gpioUnresetEnableClock(GPIO *port);
+
 void writeOne(int pinNumber, GPIO_TypeDef *port);
 void writeZero(int pinNumber, GPIO_TypeDef *port);
 
